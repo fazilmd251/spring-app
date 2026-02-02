011f08a39616301b9530ff4bf0c39d66c08d7d93
Mohamed Fazil <mohamedfazil152@gmail.com>
2026-02-02 16:47:51 +0530

    service implemented

---
+++ src/main/java/com/fazil/my_first_app/company/impl/CompanyServiceImp.java
@@ -0,0 +1,51 @@
+package com.fazil.my_first_app.company.impl;
+
+import com.fazil.my_first_app.company.Company;
+import com.fazil.my_first_app.company.CompanyRepository;
+import com.fazil.my_first_app.company.CompanyService;
+
+import java.util.List;
+
+public class CompanyServiceImp implements CompanyService {
+
+    private final CompanyRepository companyRepository;
+
+    public CompanyServiceImp(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    @Override
+    public List<Company> getCompanies() {
+        return this.companyRepository.findAll();
+    }
+
+    @Override
+    public Company getCompany(Long id) {
+        return this.companyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Company createCompany(Company company) {
+        return this.companyRepository.save(company);
+    }
+
+    @Override
+    public Company updateCompany(Company company, Long id) {
+        Company toUpdate = this.companyRepository.findById(id).orElse(null);
+        if (toUpdate != null) {
+            toUpdate.setName(company.getName());
+            return this.companyRepository.save(toUpdate);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean deleteCompany(Long id) {
+        try {
+            this.companyRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
